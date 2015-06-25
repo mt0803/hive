b13c0cd6bce3423e0b4644fd83d3041aaa7c3f32
mt0803 <540448349@qq.com>
2015-06-25 14:25:47 +0800

    clean pro

--- ql/src/java/org/apache/hadoop/hive/ql/io/parquet/convert/ConverterParent.java
+++
@@ -1,7 +0,0 @@
-package org.apache.hadoop.hive.ql.io.parquet.convert;
-
-import org.apache.hadoop.io.Writable;
-
-interface ConverterParent {
-  void set(int index, Writable value);
-}
--- ql/src/test/queries/clientpositive/tez_multi_union.q
+++
@@ -1,8 +0,0 @@
-select key from
-(
-select key from src
-union all
-select key from src
-) tab group by key
-union all
-select key from src;
